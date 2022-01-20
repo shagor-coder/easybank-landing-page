cff1de7dc44f931208be6939ca055ed2c021a9f4
Md Shagor Hossain <79301601+shagor-coder@users.noreply.github.com>
2022-01-21 01:50:18 +0600

    script update

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,26 @@
-const bodyActive=document.querySelector("body"),mobileMenu=document.querySelector(".nav__menu"),hamBurger=document.querySelector(".nav__toggle"),overlay=document.querySelector(".nav"),navLinks=document.querySelectorAll(".nav__menu__links");function menuToggle(){hamBurger.classList.toggle("active"),overlay.classList.toggle("active"),mobileMenu.classList.toggle("active")}hamBurger.addEventListener("click",(()=>{hamBurger.classList.contains("active")?bodyActive.classList.add("active"):bodyActive.classList.remove("active")}));const mainHeader=document.querySelector(".header");window.addEventListener("scroll",(()=>{mainHeader.classList.toggle("active",scrollY>0)}));
+const bodyActive = document.querySelector('body')
+const mobileMenu = document.querySelector('.nav__menu')
+const hamBurger = document.querySelector('.nav__toggle')
+const overlay = document.querySelector('.nav')
+const navLinks = document.querySelectorAll('.nav__menu__links')
+
+hamBurger.addEventListener('click', () => {
+  hamBurger.classList.toggle('active')
+  overlay.classList.toggle('active')
+  mobileMenu.classList.toggle('active')
+  bodyActive.classList.toggle('active')
+  navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      bodyActive.classList.remove('active')
+      hamBurger.classList.remove('active')
+      overlay.classList.remove('active')
+      mobileMenu.classList.remove('active')
+    })
+  })
+})
+
+const mainHeader = document.querySelector('.header')
+window.addEventListener('scroll', () => {
+  mainHeader.classList.toggle('active', scrollY > 0)
+})
 //# sourceMappingURL=script.js.map
